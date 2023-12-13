c2b0ce5c7bc8ba4281fdd87f8b7c8178617a26a3
voiteckheir <wojceichdziecic@gmail.com>
2023-12-13 16:56:07 +0100

    Dystrybucja 1

--- src/main/java/pwo/app/Sequence.java
+++ src/main/java/pwo/app/Sequence.java
@@ -1,8 +1,10 @@
 package pwo.app;
-
 public class Sequence {
-
-    public static void main(String[] args) {
-        (new SeqToFileApp()).run(args);
-    }
+public static void main(String[] args) {
+switch(args.length){
+case 3: (new SeqToOutApp()).run(args); break;
+case 4: (new SeqToFileApp()).run(args); break;
+default: System.out.println("!Illegal argument3s\n" + "Legal usage: seqName from to [fileName]");
+}
+}
 }
